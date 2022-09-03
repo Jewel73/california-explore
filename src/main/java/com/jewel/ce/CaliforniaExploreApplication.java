411b1589ee399ab9b01a07b1eef37f3e2b452d8a
jewel <jewelranar420@gmail.com>
2022-09-04 00:27:39 +0600

    first project setup

---
+++ src/main/java/com/jewel/ce/CaliforniaExploreApplication.java
@@ -0,0 +1,13 @@
+package com.jewel.ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliforniaExploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaliforniaExploreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jewel/ce/CaliforniaExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewel.ce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliforniaExploreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
